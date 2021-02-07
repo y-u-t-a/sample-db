09b722012472ffab36a0d051e3d2a3abf2d640cc
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-02-07 14:27:10 +0900

    __dict__ で dict に変換できた

--- library-db/mysql/script/python/books.py
+++ library-db/mysql/script/python/books.py
@@ -8,20 +8,12 @@ class Book:
     self.name = name
     self.author = author
 
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "publisher_id": self.publisher_id,
-      "name": self.name,
-      "author": self.author
-    }
-
 class Commands():
   def get_all_books(self):
     sql = "SELECT * FROM books"
     table = db.execute_select(sql)
     books = [Book(*row) for row in table] # タプルを展開してコンストラクタの引数に渡す
-    [print(book.to_dict()) for book in books]
+    [print(book.__dict__) for book in books]
 
 if __name__ == '__main__':
   fire.Fire(Commands)
