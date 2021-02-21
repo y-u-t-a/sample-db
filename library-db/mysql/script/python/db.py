1991198863e7e5f1b862827e377ec5543f988c60
Yuta Ueno <30320735+y-u-t-a@users.noreply.github.com>
2021-02-21 16:01:53 +0900

    Bulk Insert 処理を追加

--- library-db/mysql/script/python/db.py
+++ library-db/mysql/script/python/db.py
@@ -16,3 +16,20 @@ def execute_select(query:str):
     with conection.cursor() as cursor:
       cursor.execute(query)
       return cursor.fetchall()
+
+def bulk_insert(insert_query:str, values:list):
+  """
+  ```python
+  query = "INSERT INTO employees (name, phone) VALUES (%s, %s)"
+  data = [
+    ('Jane','555-001'),
+    ('Joe', '555-001'),
+    ('John', '555-003')
+  ]
+  db.bulk_insert(query, data)
+  ```
+  """
+  with get_connection() as conection:
+    with conection.cursor() as cursor:
+      cursor.executemany(insert_query, values)
+    conection.commit()
